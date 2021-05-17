5bfa6a4cc60a91f69de86afad72d5ea951148692
JoSGomes <engcgabrielgomes@gmail.com>
2021-05-17 18:27:52 -0300

    add post in SQL DB sensors response

--- NodeMCUCode/NodeMCUCode.ino
+++ NodeMCUCode/NodeMCUCode.ino
@@ -1,12 +1,30 @@
-/* Biblioteca para capturar o timestamp*/
 #include <stdio.h>
 #include <string.h>
+#include <TimeLib.h>
 
 /*Bibliotecas para fazer a comunicação com o MQTT usando o broker do AWS*/
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <NTPClient.h>
+
+/******************************************************************************/
+/*****VARIAVEIS E CONSTANTES DEFINIDAS PARA A CAPTURA DO TIMESTAMP:***********/
+/*****************************************************************************/
+//Servidor NTP usado para o timestemp
+static const char ntpServerName[] = "us.pool.ntp.org";
+unsigned int localPort = 8888;
+//Time zone de São Paulo, Brasil
+const int timeZone = -3;
+
+//instancia de um objeto WiFiUDP
+WiFiUDP Udp;
+
+
+//buffer para armazenar os pacotes de entrada e saída
+byte packetBuffer[NTP_PACKET_SIZE];
+
+//FIM DAS VARIAVEIS DE TIMESTAMP
 
 /*Arquivo contendo usuário e senha do MySQL*/
 #include "arduino_secrets.h"
@@ -23,11 +41,9 @@
 
 //Pino do botao da placa
 #define BUTTON D3
-#define TAMFILES 4//tamanho definido no txt para admitir a rotina
+#define TAMFILES 8//tamanho definido no txt para admitir a rotina
 File fA, fG;
 
-unsigned int localPort = 8888;
-static const char ntpServerName[] = "us.pool.ntp.org";
 //Nome e senha da rede WiFi:
 const char * ssid = USER_WIFI;
 const char * password = PASSWORD_WIFI;
@@ -38,7 +54,7 @@ void setupWifi(); //Procedimento para conectar a placa ao WiFi.
 void reconnect(); //Procedimento para conectar a placa ao MQTT.
 void carregarArquivos(); // Procedimento que carrega os arquivos de certificados do AWS.
 void sendNTPpacket(IPAddress &address);
-void enviarEvento(int day_, int month_, char * description);
+void enviarEvento(char * hour_, int day_, int month_, char * name_,char * description);
 
 time_t getNtpTime();
 
@@ -63,7 +79,7 @@ char user[] = SECRET_USERDB;
 char pass[] = SECRET_PASSDB;
 
 //Comando para mandar os dados para o banco de dados:
-char INSERT_SQL_EVENTS[] = "INSERT INTO pbl.events (day, month, description) VALUES ('%d','%d','%s')";
+char INSERT_SQL_EVENTS[] = "INSERT INTO pbl.events (hour, day, month, name, description) VALUES ('%s','%d','%d','%s','%s')";
 char query[128];
 
 
@@ -86,6 +102,7 @@ int intervalAlarm;//tempo limite para quando está parado, se antigido, então a
 int loopSensors = 0;//contador utilizado para variar sequenciamente no Loop o valor dos sensores com base no TXT.
 
 void setup() {
+  
   laterMillis = millis();//captura o primeiro milli de inicialização da placa.
   Serial.begin(115200); //inicia o display serial, para depuração.
   pinMode(LED_BUILTIN, OUTPUT); // inicializa o pino do led como saída.
@@ -103,7 +120,7 @@ void setup() {
     Serial.println("Conectando SQL novamente.");
   }
   Serial.println("Conectado ao servidor SQL."); 
-
+  
   delay(1000);
   
   //faz o carregamentos dos certificados no espClient:
@@ -113,7 +130,12 @@ void setup() {
   //Conecta a placa ao MQTT
   reconnect();
   
-  
+   //inicia o objeto Udp na portal local:
+  Udp.begin(localPort);
+
+  //Faz a sincronização do provedor para capturar o timestemp correto:
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
 }
 
 void loop() {
@@ -135,7 +157,6 @@ void loop() {
     }
     else{
       digitalWrite(LED_BUILTIN, LOW);
-      enviarEvento(5, 15, "Apenas um teste.");//está aqui somente para teste
       alarm = true;
       
     }
@@ -148,12 +169,35 @@ void loop() {
   }
 
   delay(500);
+  char hour_[7];
+  sprintf(hour_, "%d:%d", hour(), minute());
   if(alarm == true){
-    if(accelerometer[0][loopSensors] != 0 || accelerometer[1][loopSensors] != 0 || accelerometer[2][loopSensors] != 0)//verifica o acelerômetro em X,Y,Z   
-      enviarEvento(16,5, "A moto está sendo roubada!!");//colocando as datas manualmente... usar a bib dps
+    if(accelerometer[0][loopSensors] != 0 || accelerometer[1][loopSensors] != 0 || (accelerometer[2][loopSensors] >=  7 && accelerometer[2][loopSensors] <= 10)  || gyroscope[0][loopSensors] != 0 || gyroscope[1][loopSensors] != 0 || gyroscope[2][loopSensors] != 0)//verifica o acelerômetro em X,Y,Z   
+     enviarEvento(hour_, day(), month(), "ASSALTO", "A moto está sendo roubada!!");//colocando as datas manualmente... usar a bib dps
   }
   else{//alarme não ativado, segue rotina normal, em movimento.
     
+    if(( gyroscope[0][loopSensors] <= -60 && gyroscope[1][loopSensors] == 0 && gyroscope[2][loopSensors] == 0) && (accelerometer[0][loopSensors] >= 6  &&  accelerometer[1][loopSensors] == 0 && accelerometer[2][loopSensors] <= 4)){           
+      enviarEvento(hour_, day(), month(), "POSSIVEL ACIDENTE", "TOMBOU PARA DIRETA");      
+    }
+    
+    else if(( gyroscope[0][loopSensors] >= 60 && gyroscope[1][loopSensors] == 0 && gyroscope[2][loopSensors] == 0) && (accelerometer[0][loopSensors] <= -6  &&  accelerometer[1][loopSensors] == 0 && accelerometer[2][loopSensors] <= 4)){
+      enviarEvento(hour_, day(), month(),  "POSSIVEL ACIDENTE", "TOMBOU PARA ESQUERDA");
+      
+    }
+    else if(( gyroscope[0][loopSensors] == 0 && gyroscope[1][loopSensors] >= 60 && gyroscope[2][loopSensors] == 0) && (accelerometer[0][loopSensors] == 0  &&  accelerometer[1][loopSensors] >= 6 && accelerometer[2][loopSensors] <= 4)){
+      enviarEvento(hour_, day(), month(),  "POSSIVEL ACIDENTE", "TOMBOU PARA FRENTE");
+      
+    }
+    else if(( gyroscope[0][loopSensors] == 0 && gyroscope[1][loopSensors] <= -60 && gyroscope[2][loopSensors] == 0) && (accelerometer[0][loopSensors] == 0  &&  accelerometer[1][loopSensors] <= -6 && accelerometer[2][loopSensors] <= 4)){
+      enviarEvento(hour_, day(), month(), "POSSIVEL ACIDENTE", "TOMBOU PARA TRÁS");
+    }
+    else if( (gyroscope[0][loopSensors] >= 360 || gyroscope[0][loopSensors] <= -360 || gyroscope[1][loopSensors] >= 360 || gyroscope[1][loopSensors] <= -360 || gyroscope[2][loopSensors] >= 360 || gyroscope[2][loopSensors] <= -360) && (accelerometer[0][loopSensors] == 0  &&  accelerometer[1][loopSensors] == 0 && accelerometer[2][loopSensors] == -10 )){
+      enviarEvento(hour_, day(), month(), "POSSIVEL ACIDENTE", "CAPOTOU");
+      Serial.println(gyroscope[0][loopSensors]);
+      Serial.println(gyroscope[1][loopSensors]);
+      Serial.println(gyroscope[2][loopSensors]);
+    }
     
   }
   
@@ -422,15 +466,82 @@ void carregarArquivos(){
 }
 
 /*
+ * Procedimento que faz a sincronização com o provedor.
+ * 
+ * return:
+ *    Retorna os segundos, desde 1970 até o instante foi chamada.
+ *    Retorna 0 se não conseguir sincronizar.
+ */
+time_t getNtpTime(){
+  IPAddress ntpServerIP; //enderço IP do servidor NTP
+
+  //descartar todos os pacotes recebidos anteriormente
+  while (Udp.parsePacket() > 0);
+  // obtém um servidor aleatório do pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+
+  //Envia uma solicitação NTP para o servidor obtido aleatóriamente:
+  sendNTPpacket(ntpServerIP);
+
+  //Permanece no loop por 1500 ms:
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      //ler o pacote no buffer Udp:
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);
+      unsigned long secsSince1900;
+      // Converte quatro bytes começando na localização 40 em um long int:
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  
+  return 0; // Retorna 0 se não for possível obter a hora
+}
+
+/*
+ * Envia uma solicitação NTP para o servidor de horário no endereço fornecido
+ * 
+ * Parâmetros:
+ *    endereço IP do servidor.
+ */
+void sendNTPpacket(IPAddress &address){
+  // Setta todos os bytes no buffer para 0:
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  
+  // Inicializa os valores necessários para formar a solicitação NTP:
+  packetBuffer[0] = 0b11100011;   // LI, versão, modo
+  packetBuffer[1] = 0;     // estrado, ou tipo de clock
+  packetBuffer[2] = 6;     // Intervalo de votação
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes de zero para Root Delay e Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+
+  //Agora, envia um pacote solicitando um carimbo de data/hora:
+  Udp.beginPacket(address, 123); // Os pedidos NTP são para a porta 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+/*
  * Envia o Estado da lampada para a tabela do banco de dados MySQL estado
  * 
  * Parâmetros:
- *    char * estado -> Estado atual da lampada.
+ *    hour_ -> hora do evento
+ *    day_ -> dia do evento
+ *    month_ ->
  *    
  */
-void enviarEvento(int day_, int month_, char * description){
+void enviarEvento(char * hour_ ,int day_, int month_, char * name_, char * description){
   //transforma os dados em um comando do MySQL:
-  sprintf(query, INSERT_SQL_EVENTS, day_, month_, description);
+  sprintf(query, INSERT_SQL_EVENTS, hour_, day_, month_, name_,description);
   // Inicia a instancia da classe de consulta:
   MySQL_Cursor * cur_mem = new MySQL_Cursor(&conn);
   //Executa a consulta (query):
